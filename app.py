1f2d8e747d59679ad033a88f4aeaa7384234a711
dada-tech <david.olu.dada@gmail.com>
2023-12-01 19:12:20 -0700

    multiple negative sentiments

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, jsonify
+import random
 
 import text_classifier
 
@@ -30,10 +31,20 @@ def create_app():
 
         if classification == 0:
             mood = "happy"
-            prediction = "Great !"
+            prediction = "Awesome! Glad you enjoyed it !"
         else:
-            mood = "angry"
-            prediction = "Sorry to hear that!"
+            # Randomly choose a negative sentiment: annoyed or sad
+            options = [0, 1]
+            weights = [0.8, 0.2]
+
+            chosen_option = random.choices(options, weights=weights, k=1)[0]
+
+            if chosen_option == 0:
+                mood = "annoyed"
+                prediction = "Sorry you didnt enjoy that movie..."
+            else:
+                mood = "angry"
+                prediction = "I agree. Lets demand a refund!"
 
         # return render_template(
         #     'index.html', prediction=prediction, mood=mood)
